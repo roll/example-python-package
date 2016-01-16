3c157d3140d50578ed0e60ef77ffa9c61a95e219
roll <roll@post.agency>
2016-01-16 12:39:33 +0300

    updated

--- activate.sh
+++ activate.sh
@@ -1,13 +1,10 @@
-# Run alias
-alias run='python run.py'
-
 # Git commit hook
 if [ ! -f .git/hooks/pre-commit ]; then
-    echo -e "#!/bin/sh\n\n\npython run.py check" > .git/hooks/pre-commit
+    echo -e "#!/bin/sh\n\n\nmario check" > .git/hooks/pre-commit
     chmod +x .git/hooks/pre-commit
 fi
 
 # Python env
 virtualenv venv
 source venv/bin/activate
-python run.py prepare
+pip install -Urrequirements.dev.txt
